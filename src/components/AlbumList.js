d35c8712665547d5074ab1945ae30cb6fd1c1775
Ryan Shook <@RyanShook>
2017-08-15 11:57:08 -0500

    7-31
    Established framework for class based component, next step is to build
    out the class based component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+//Create a class based component
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album List!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
