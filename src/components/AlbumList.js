da0b7e67b3b44fcd88c9642703636a0b5c5cc980
Ryan Shook <@RyanShook>
2017-08-30 12:43:32 -0500

    7-37
    Began using setState with class based component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-//Create a class based component
+//Create a class based component with level states
 class AlbumList extends Component {
+//Initialized empty state
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data }));
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>Album List!!</Text>
